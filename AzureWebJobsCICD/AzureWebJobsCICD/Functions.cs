18fddba7433d1a77ec293f9a77cce3080f3ec3a4
anaparias <anaparias2016@gmail.com>
2024-08-29 16:13:16 -0300

    initial commit

---
+++ AzureWebJobsCICD/AzureWebJobsCICD/Functions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace AzureWebJobsCICD
+{
+    public class Functions
+    {
+        [NoAutomaticTrigger]
+        public static void ProcessQueueMessage(ILogger logger)
+        {
+            logger.LogInformation($"Test Job is Running");
+        }
+    }
+}
