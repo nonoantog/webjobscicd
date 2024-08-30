43fb07ba465de644afe996ac432af83c41928285
anaparias <anaparias2016@gmail.com>
2024-08-30 18:25:21 -0300

    add a new web job

---
+++ AzureWebJobsCICD/AzureWebJobsCICD_2/Functions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace AzureWebJobsCICD_2
+{
+    public class Functions
+    {
+        [NoAutomaticTrigger]
+        public static void ProcessQueueMessage(ILogger logger)
+        {
+            logger.LogInformation($"Test Job 2 is Running");
+        }
+    }
+}
---
+++ AzureWebJobsCICD/AzureWebJobsCICD_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace AzureWebJobsCICD_2
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?linkid=2250384
+    internal class Program
+    {
+        // Please set AzureWebJobsStorage connection strings in appsettings.json for this WebJob to run.
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureWebJobs(b =>
+                {
+                    b.AddAzureStorageCoreServices()
+                    .AddAzureStorageQueues();
+                })
+                .ConfigureLogging((context, b) =>
+                {
+                    b.SetMinimumLevel(LogLevel.Information);
+                    b.AddConsole();
+                });
+
+
+            var host = builder.Build();
+            using (host)
+            {
+                // The following code ensures that the WebJob will be running continuously
+                await host.RunAsync();
+            }
+        }
+    }
+}
+
+
